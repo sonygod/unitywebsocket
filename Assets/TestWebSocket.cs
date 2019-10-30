4a005abbdaae14044db077a278e47a715ac2daad
sonygod <sonygodx@gmail.com>
2019-10-30 11:15:24 +0800

    firsttime

---
+++ Assets/TestWebSocket.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using haxe.lang;
+using UnityEngine;
+
+using haxe.root;
+using UnityEditor.Callbacks;
+using UnityEngine.UI;
+using web.proto;
+
+public class TestWebSocket : MonoBehaviour {
+   
+
+    public string Text;
+    // Use this for initialization
+	void Start ()
+    {
+
+    TestPackBuilder.main();
+
+
+      Client.main();
+     
+
+    }
+
+    
+  
+}
