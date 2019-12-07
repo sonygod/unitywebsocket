01736afc9d2245518599348693e65c4764d0853b
sonygod <sonygodx@gmail.com>
2019-12-07 22:57:36 +0800

    111

---
+++ Assets/libs/hq/TinyBuyItemCS.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+namespace web
+{
+  public  class TinyBuyItemCS
+    {
+        public int id;
+        public int uid;
+        public int coins;
+        public string ip;
+        public string create_time;
+
+    }
+}
